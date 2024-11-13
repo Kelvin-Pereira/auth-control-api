5c9d6a689c6eb3e67c4ac1cb100c9152af26a5dc
Kelvin Pereira Biserra <42652204+Kelvin-Pereira@users.noreply.github.com>
2024-11-12 22:20:13 -0300

    Adicionar estrutura de projeto inicial com Maven Wrapper
    Este commit configura a estrutura básica para o projeto `auth-control-api`. Ele inclui scripts Maven Wrapper, o `pom.xml` para dependências, classe de aplicativo Spring Boot primária e arquivos de configuração básicos como `.gitignore` e `application.properties`.

---
+++ src/main/java/com/koldex/auth/AuthControlApiApplication.java
@@ -0,0 +1,13 @@
+package com.koldex.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthControlApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koldex/auth/AuthControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koldex.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
